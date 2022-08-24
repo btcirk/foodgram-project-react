806fb956ad99e01172f577448920583ffef2bd26
vvb <vvb@playtronix.net>
2022-08-25 06:44:21 +0800

    Add custom User model

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# import users.models
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -27,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +84,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 'admin'
+    USER = 'user'
+
+    ROLE_CHOICES = (
+        (ADMIN, 'admin'),
+        (USER, 'user'),
+    )
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    role = models.CharField('Роль пользователя',
+                            max_length=5,
+                            choices=ROLE_CHOICES,
+                            default=USER)
+    confirmation_code = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.username
