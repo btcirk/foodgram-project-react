dcde8da44511c5b57d6ea7a3ba05f7c5986da5f0
Василий Бычков <btcirk@yandex.ru>
2022-09-05 18:10:27 +0800

    Download cart

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,9 @@
-import os.path
-from foodgram.settings import MEDIA_ROOT
-
-from rest_framework import viewsets, mixins, status, filters
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import action
@@ -13,20 +15,6 @@ from .serializers import RecipeSerializer, RecipeMiniSerializer
 from .filters import RecipeFilter, IngredientsFilter
 from .permissions import Owner
 from api.pagination import LimitPageNumberPagination
-
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
-
-from borb.pdf.canvas.layout.list.unordered_list import UnorderedList
-from borb.pdf.canvas.layout.page_layout.multi_column_layout import SingleColumnLayout
-from borb.pdf.canvas.layout.page_layout.page_layout import PageLayout
-from borb.pdf.canvas.layout.text.paragraph import Paragraph
-from borb.pdf.document.document import Document
-from borb.pdf.page.page import Page
-from borb.pdf.pdf import PDF
-from django.http import HttpResponse
-import uuid
 
 
 class ListRetrieveViewSet(mixins.ListModelMixin,
@@ -64,84 +52,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(detail=True, methods=['post', 'delete'],
-            permission_classes=[IsAuthenticated])
-    def favorite(self, request, pk=None):
-        if request.method == 'POST':
-            return self.add_obj(Favorites, request.user, pk)
-        elif request.method == 'DELETE':
-            return self.delete_obj(Favorites, request.user, pk)
-        return None
-
-    @action(detail=True, methods=['post', 'delete'],
-            permission_classes=[IsAuthenticated])
-    def shopping_cart(self, request, pk=None):
-        if request.method == 'POST':
-            return self.add_obj(Cart, request.user, pk)
-        elif request.method == 'DELETE':
-            return self.delete_obj(Cart, request.user, pk)
-        return None
-
-    def generate_pdf(self, shopping_list):
-        doc: Document = Document()
-        page: Page = Page()
-        doc.add_page(page)
-        layout: PageLayout = SingleColumnLayout(page)
-        for item in shopping_list:
-            str = f'{item} ({shopping_list[item]["measurement_unit"]}) - {shopping_list[item]["amount"]}'
-            layout.add(UnorderedList().add(Paragraph(str, font="Courier")))
-        filename = str(uuid.uuid4())
-        full_path = os.path.join(MEDIA_ROOT, filename)
-        print(full_path)
-        #with open(filename, "wb") as pdf_file_handle:
-        #    PDF.dumps(pdf_file_handle, doc)
-        return filename
-
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
-    def download_shopping_cart(self, request):
-        ingredients_list = {}
-        ingredients = IngredientAmount.objects.filter(
-            recipe__cart__user=request.user).values_list(
-            'ingredient__name', 'ingredient__measurement_unit',
-            'amount')
-        for item in ingredients:
-            name = item[0]
-            if name not in ingredients_list:
-                ingredients_list[name] = {
-                    'measurement_unit': item[1],
-                    'amount': item[2]
-                }
-            else:
-                ingredients_list[name]['amount'] += item[2]
-        pdfmetrics.registerFont(
-            TTFont('OpenSans', 'OpenSans-Regular.ttf', 'UTF-8'))
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = ('attachment; '
-                                           'filename="shopping_list.pdf"')
-        page = canvas.Canvas(response)
-        page.setFont('OpenSans', size=24)
-        page.drawString(200, 800, 'Список ингредиентов')
-        page.setFont('OpenSans', size=16)
-        height = 750
-        for i, (name, data) in enumerate(ingredients_list.items(), 1):
-            page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
-                                         f'{data["measurement_unit"]}'))
-            height -= 25
-        page.showPage()
-        page.save()
-        return response
-        #print(ingredients_list.keys())
-        #print(type(ingredients_list.values()))
-        #for item in ingredients_list:
-        #    print(f'{item} ({ingredients_list[item]["measurement_unit"]}) - {ingredients_list[item]["amount"]}')
-        #response = HttpResponse(content_type='application/pdf')
-        #response['Content-Disposition'] = ('attachment; '
-        #                                   'filename="shopping_list.pdf"')
-
-        #return response
-        #self.generate_pdf(ingredients_list)
-        #return Response(ingredients_list, status=status.HTTP_200_OK)
-
     def add_obj(self, model, user, pk):
         if model.objects.filter(user=user, recipe__id=pk).exists():
             return Response({
@@ -160,3 +70,57 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response({
             'errors': 'Рецепт уже удален'
         }, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=[IsAuthenticated])
+    def favorite(self, request, pk=None):
+        if request.method == 'POST':
+            return self.add_obj(Favorites, request.user, pk)
+        elif request.method == 'DELETE':
+            return self.delete_obj(Favorites, request.user, pk)
+        return None
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=[IsAuthenticated])
+    def shopping_cart(self, request, pk=None):
+        if request.method == 'POST':
+            return self.add_obj(Cart, request.user, pk)
+        elif request.method == 'DELETE':
+            return self.delete_obj(Cart, request.user, pk)
+        return None
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def download_shopping_cart(self, request):
+        i_list = {}
+        ingredients = IngredientAmount.objects.filter(
+            recipe__cart__user=request.user).values_list(
+            'ingredient__name', 'ingredient__measurement_unit',
+            'amount')
+        for item in ingredients:
+            name = item[0]
+            if name not in i_list:
+                i_list[name] = {
+                    'measurement_unit': item[1],
+                    'amount': item[2]
+                }
+            else:
+                i_list[name]['amount'] += item[2]
+        pdfmetrics.registerFont(
+            TTFont('OpenSans', 'OpenSans-Regular.ttf', 'UTF-8'))
+        buffer = io.BytesIO()
+        p = canvas.Canvas(buffer)
+        p.setFont('OpenSans', size=24)
+        p.drawString(200, 800, 'Список ингредиентов')
+        p.setFont('OpenSans', size=16)
+        height = 750
+        for item in i_list:
+            p.drawString(75, height, (f'- {item} '
+                                      f'({i_list[item]["measurement_unit"]})'
+                                      f' - {i_list[item]["amount"]}'))
+            height -= 25
+        p.drawString(100, 100, "Hello world.")
+        p.showPage()
+        p.save()
+
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename='ingredients_list.pdf')
