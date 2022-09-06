12aa3d93b760ea7fb53fc725540bf2957c0daa4b
vvb <vvb@playtronix.net>
2022-09-06 18:51:24 +0800

    isort

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from users.models import Subscription, User
 from recipes.models import (Recipe, Tag, Ingredient, IngredientAmount,
                             Cart, Favorites)
